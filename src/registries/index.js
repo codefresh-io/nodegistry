e52746eb6138ec7458b99b2233d1c70d457757bc
Vadim Kharin <vadim.kharin@codefresh.io>
2021-07-27 18:10:00 +0300

    CR-3446 Support Managed Identities for ACR

--- src/registries/index.js
+++ src/registries/index.js
@@ -4,10 +4,12 @@ const StandardRegistry = require('./StandardRegistry');
 const DockerhubRegistry = require('./DockerhubRegistry');
 const EcrRegistry = require('./EcrRegistry');
 const GcrRegistry = require('./GcrRegistry');
+const AcrRegistry = require('./AcrRegistry');
 
 module.exports = {
     StandardRegistry,
     DockerhubRegistry,
     EcrRegistry,
     GcrRegistry,
+    AcrRegistry,
 };
